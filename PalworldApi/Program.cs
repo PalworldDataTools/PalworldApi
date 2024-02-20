48e9734fdc4279f1d498d9b9f6b0963500b8c7ae
Ismail Bennani <lahkim.bennani.ismail@gmail.com>
2024-02-20 20:47:39 +0100

    add serilog

--- PalworldApi/Program.cs
+++ PalworldApi/Program.cs
@@ -3,61 +3,81 @@ using PalworldApi;
 using PalworldApi.Rest.v1;
 using PalworldApi.Serialization;
 using PalworldApi.Services;
+using Serilog;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration().Enrich.FromLogContext().WriteTo.Console().CreateBootstrapLogger();
 
-builder.Services.AddProblemDetails();
-builder.Services.AddCors(opt => opt.AddDefaultPolicy(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()));
-builder.Services.AddResponseCaching();
+try
+{
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
+    builder.Services.AddProblemDetails();
+    builder.Services.AddCors(opt => opt.AddDefaultPolicy(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()));
+    builder.Services.AddResponseCaching();
 
-builder.Services.AddSingleton<AppJsonSerializerContext>();
-builder.Services.ConfigureHttpJsonOptions(options => { options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default); });
+    builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
 
-builder.Services.AddSingleton<RawDataService>(provider => new RawDataService(provider.GetRequiredService<ILogger<RawDataService>>()));
+    builder.Services.AddSingleton<AppJsonSerializerContext>();
+    builder.Services.ConfigureHttpJsonOptions(options => { options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default); });
 
-builder.Services.AddMvc();
-builder.Services.AddControllers(
-    opt =>
+    builder.Services.AddSingleton<RawDataService>(provider => new RawDataService(provider.GetRequiredService<ILogger<RawDataService>>()));
+
+    builder.Services.AddMvc();
+    builder.Services.AddControllers(
+        opt =>
+        {
+            opt.CacheProfiles.Add(
+                Constants.ResponseCacheDefaultProfile,
+                new CacheProfile { Duration = (int)TimeSpan.FromMinutes(1).TotalSeconds, Location = ResponseCacheLocation.Any }
+            );
+            opt.CacheProfiles.Add(
+                Constants.ResponseCacheLongTermProfile,
+                new CacheProfile { Duration = (int)TimeSpan.FromDays(1).TotalSeconds, Location = ResponseCacheLocation.Any }
+            );
+        }
+    );
+    builder.Services.AddEndpointsApiExplorer();
+
+    builder.Services.AddV1();
+
+    builder.Logging.ClearProviders();
+    builder.Logging.AddAzureWebAppDiagnostics();
+
+    builder.Host.UseSerilog((context, services, configuration) => configuration.ReadFrom.Configuration(context.Configuration).ReadFrom.Services(services), writeToProviders: true);
+
+    WebApplication app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
     {
-        opt.CacheProfiles.Add(
-            Constants.ResponseCacheDefaultProfile,
-            new CacheProfile { Duration = (int)TimeSpan.FromMinutes(1).TotalSeconds, Location = ResponseCacheLocation.Any }
-        );
-        opt.CacheProfiles.Add(Constants.ResponseCacheLongTermProfile, new CacheProfile { Duration = (int)TimeSpan.FromDays(1).TotalSeconds, Location = ResponseCacheLocation.Any });
+        app.UseDeveloperExceptionPage();
+        app.UseStatusCodePages();
     }
-);
-builder.Services.AddEndpointsApiExplorer();
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
 
-builder.Services.AddV1();
+    app.UseOpenApi();
+    app.UseSwaggerUi();
 
-builder.Logging.AddAzureWebAppDiagnostics();
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseRouting();
 
-WebApplication app = builder.Build();
+    app.UseCors();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseStatusCodePages();
+    app.UseResponseCaching();
+
+    app.MapDefaultControllerRoute();
+
+    app.Run();
 }
-else
+catch (Exception exn)
 {
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+    Log.Fatal(exn, "Unhandled exception.");
 }
-
-app.UseOpenApi();
-app.UseSwaggerUi();
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-
-app.UseCors();
-
-app.UseResponseCaching();
-
-app.MapDefaultControllerRoute();
-
-app.Run();
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
