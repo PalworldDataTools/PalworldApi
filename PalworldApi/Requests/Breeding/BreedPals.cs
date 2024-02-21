8ac7616f1b646049fa5c7e09cf860637ae28ba4b
Ismail Bennani <lahkim.bennani.ismail@gmail.com>
2024-02-21 22:57:54 +0100

    add breed pals api

---
+++ PalworldApi/Requests/Breeding/BreedPals.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using MediatR;
+using PalworldDataExtractor.Abstractions;
+using PalworldDataExtractor.Abstractions.Pals;
+
+namespace PalworldApi.Requests.Breeding;
+
+class BreedPals : IRequestHandler<BreedPalsRequest, Pal>
+{
+    public Task<Pal> Handle(BreedPalsRequest request, CancellationToken cancellationToken)
+    {
+        if (request.PalA.TribeName != null
+            && request.PalB.TribeName != null
+            && TryFindUniqueCombination(request.Data, request.PalA.TribeName, request.PalB.TribeName, out string? child))
+        {
+            if (!TryFindPal(request.Data, child, out Pal? childPal))
+            {
+                throw new Exception($"Could not find pal with name {child}");
+            }
+
+            return Task.FromResult(childPal);
+        }
+
+        int combiRankA = request.PalA.CombiRank;
+        int combiRankB = request.PalB.CombiRank;
+        int mean = (int)Math.Floor((float)(combiRankA + combiRankB + 1) / 2);
+        Pal closestPal = request.Data.Tribes.SelectMany(t => t.Pals).Where(p => p is { IsBoss: false, IsTowerBoss: false }).OrderBy(p => Math.Abs(p.CombiRank - mean)).First();
+
+        return Task.FromResult(closestPal);
+    }
+
+    static bool TryFindUniqueCombination(ExtractedData data, string tribeNameA, string tribeNameB, [NotNullWhen(true)] out string? result)
+    {
+        result = data.UniqueBreedingCombinations.FirstOrDefault(c => c.ParentTribeA == tribeNameA && c.ParentTribeB == tribeNameB)?.ChildCharacterId;
+        return result != null;
+    }
+
+    static bool TryFindPal(ExtractedData data, string palName, [NotNullWhen(true)] out Pal? pal)
+    {
+        pal = data.Tribes.SelectMany(t => t.Pals).FirstOrDefault(t => t.Name == palName);
+        return pal != null;
+    }
+}
+
+class BreedPalsRequest : IRequest<Pal>
+{
+    public required ExtractedData Data { get; init; }
+    public required Pal PalA { get; init; }
+    public required Pal PalB { get; init; }
+}
---
+++ PalworldApi/Rest/v1/Controllers/PalBreedingController.cs
@@ -0,0 +1,79 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
+using PalworldApi.Requests.Breeding;
+using PalworldApi.Rest.OpenApi;
+using PalworldApi.Rest.v1.Models.Pals;
+using PalworldApi.Services;
+using PalworldDataExtractor.Abstractions;
+using PalTribe = PalworldDataExtractor.Abstractions.Pals.PalTribe;
+
+namespace PalworldApi.Rest.v1.Controllers;
+
+/// <summary>
+///     Get data about breeding
+/// </summary>
+[ApiController]
+[Route("v1/pals/breed")]
+[OpenApiTag("Breeding")]
+[OpenApiVersion("v1")]
+[ResponseCache(CacheProfileName = Constants.ResponseCacheDefaultProfile)]
+public class PalBreedingController : ControllerBase
+{
+    readonly RawDataService _rawDataService;
+    readonly IMediator _mediator;
+
+    /// <summary>
+    ///     Create the pal breeding controller
+    /// </summary>
+    public PalBreedingController(RawDataService rawDataService, IMediator mediator)
+    {
+        _rawDataService = rawDataService;
+        _mediator = mediator;
+    }
+
+    /// <summary>
+    ///     Get breeding result
+    /// </summary>
+    /// <param name="palNameA">The name of the first pal</param>
+    /// <param name="palNameB">The name of the second pal</param>
+    /// <returns>The name of the tribe that is the result of the breeding</returns>
+    [HttpGet]
+    [ProducesResponseType<PalTribe>(StatusCodes.Status200OK)]
+    [ProducesResponseType<ProblemHttpResult>(StatusCodes.Status404NotFound)]
+    public async Task<Results<Ok<Pal>, ProblemHttpResult>> GetBreedingResult(string palNameA, string palNameB)
+    {
+        ExtractedData? data = await _rawDataService.GetData(RawDataService.DefaultVersion);
+        if (data == null)
+        {
+            return DataNotFound(RawDataService.DefaultVersion);
+        }
+
+        if (!TryFindPal(data, palNameA, out PalworldDataExtractor.Abstractions.Pals.Pal? palA))
+        {
+            return PalNotFound(palNameA);
+        }
+
+        if (!TryFindPal(data, palNameB, out PalworldDataExtractor.Abstractions.Pals.Pal? palB))
+        {
+            return PalNotFound(palNameB);
+        }
+
+        PalworldDataExtractor.Abstractions.Pals.Pal child = await _mediator.Send(new BreedPalsRequest { Data = data, PalA = palA, PalB = palB });
+
+        return TypedResults.Ok(child.ToV1());
+    }
+
+    static bool TryFindPal(ExtractedData data, string palName, [NotNullWhen(true)] out PalworldDataExtractor.Abstractions.Pals.Pal? pal)
+    {
+        pal = data.Tribes.SelectMany(t => t.Pals).FirstOrDefault(t => t.Name == palName);
+        return pal != null;
+    }
+
+    static ProblemHttpResult PalNotFound(string name, string? message = null) =>
+        TypedResults.Problem($"{message ?? "Could not find pal with name"}: {name}", statusCode: StatusCodes.Status404NotFound);
+
+    static ProblemHttpResult DataNotFound(string version) => TypedResults.Problem($"Could not find data for version: {version}", statusCode: StatusCodes.Status404NotFound);
+}
